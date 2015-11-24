438bff7ef5f6989ace44ca3dde999cafebaea67b
Danil Pismenny <danil@brandymint.ru>
2015-11-24 11:08:22 +0300

    a.gif -> r.gif

--- app/controllers/receiver_controller.rb
+++ app/controllers/receiver_controller.rb
@@ -4,7 +4,7 @@ class ReceiverController < ApplicationController
 
     EventCreator.create! params: data, request: request
 
-    send_file Rails.root.join('public/a.gif')
+    send_file Rails.root.join('public/r.gif')
   end
 
   private
