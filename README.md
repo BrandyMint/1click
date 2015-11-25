34913149df151799f084f2a1244e083ef90ec710
Danil Pismenny <danil@brandymint.ru>
2015-11-25 09:02:30 +0300

    app users controller

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # aristotel
+
+
+## Библиотеки для отображения воронки
+
+* http://jakezatecky.github.io/d3-funnel/
+* https://github.com/stefanwalther/sense-funnel-chart
+* http://www.amcharts.com
+* http://www.fusioncharts.com
--- app/controllers/app_user_controller.rb
+++ app/controllers/app_user_controller.rb
@@ -1,1 +1,9 @@
-AZZZZ
+class AppUsersController < ApplicationController
+  def index
+    render locals: { users: app_users.order('created_at desc') }
+  end
+
+  def show
+    render locals: { app_user: app_users.where(userId: params[:id]).first! }
+  end
+end
