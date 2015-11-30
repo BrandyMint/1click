4d8e106d8695b797dd197fcb5d217b2b335723ff
Danil Pismenny <danil@brandymint.ru>
2015-11-30 11:51:29 +0300

    secrets example

--- app/controllers/app_user_controller.rb
+++
@@ -1,9 +0,0 @@
-class AppUsersController < ApplicationController
-  def index
-    render locals: { users: app_users.order('created_at desc') }
-  end
-
-  def show
-    render locals: { app_user: app_users.where(userId: params[:id]).first! }
-  end
-end
