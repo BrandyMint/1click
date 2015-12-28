2b9d3919bd4294dbbcfc9b2a621a854b0905cd0d
Denis Khabarov <admin@saymon21-root.pro>
2015-12-28 09:56:08 +0300

    sidekiq role

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,7 +3,7 @@ set :stage, :production
 set :repo_url, 'git@github.com:BrandyMint/1click.git'
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
 set :deploy_to, -> { "/home/wwwoneclickanalytics/#{fetch(:application)}" }
-server 'icfdev.ru', user: 'wwwoneclickanalytics', port: 255, roles: %w(web app db)
+server 'icfdev.ru', user: 'wwwoneclickanalytics', port: 255, roles: %w(web app db sidekiq)
 set :rails_env, :production
 set :branch, ENV['BRANCH'] || 'master'
 fetch(:default_env).merge!(rails_env: :production)
