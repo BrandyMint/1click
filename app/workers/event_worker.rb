74087fd3503ccec6e4663469ae7d694efdd91b4b
Danil Pismenny <danil@brandymint.ru>
2015-12-31 17:21:05 +0300

    fix EventWorker

--- app/workers/event_worker.rb
+++ app/workers/event_worker.rb
@@ -1,4 +1,7 @@
 class EventWorker
+  include Sidekiq::Worker
+  sidekiq_options queue: :default
+
   def perform(user_agent:, query_string:)
     EventCreator.create! user_agent: user_agent, query_string: query_string
   end
