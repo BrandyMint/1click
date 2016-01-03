54c7e3198c4710f142518be4632eb7f08a1361c7
Danil Pismenny <danil@brandymint.ru>
2016-01-03 20:28:02 +0300

    minor refactor sidekiq

--- app/controllers/receiver_controller.rb
+++ app/controllers/receiver_controller.rb
@@ -5,7 +5,7 @@ class ReceiverController < ApplicationController
 
   def create
     # DO Nothing
-    EventWorker.perform_async user_agent: request.user_agent, query_string: request.query_string
+    EventWorker.perform_async request.user_agent, request.query_string
     send_file Rails.root.join("public/r.#{params[:format]}"), filename: "a.#{params[:format]}"
   rescue => err
     Bugsnag.notify err
--- app/workers/event_worker.rb
+++ app/workers/event_worker.rb
@@ -2,7 +2,7 @@ class EventWorker
   include Sidekiq::Worker
   sidekiq_options queue: :default
 
-  def perform(user_agent:, query_string:)
+  def perform(user_agent, query_string)
     EventCreator.create! user_agent: user_agent, query_string: query_string
   end
 end
