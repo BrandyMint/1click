29930f92cc9c61337e4b58b164cebb2406f3e2a9
Danil Pismenny <danil@brandymint.ru>
2016-01-03 20:42:54 +0300

    fix sidekiq initializers

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,20 +1,6 @@
 require 'sidekiq'
-CONFIG_FILE = './config/crontab.yml'
 
-if Rails.env.production?
-  Sidekiq.configure_server do |config|
-    # Sidekiq::Logging.logger = LoggerCreator.build :sidekiq
-    # config.redis = Secrets.sidekiq.redis.symbolize_keys
-    config.error_handlers << proc { |ex, context| Bugsnag.notify(ex, context) }
-    config.failures_max_count = 50
-    config.failures_default_mode = :exhausted
-    # Sidekiq::Cron::Job.load_from_hash YAML.load_file(CONFIG_FILE)
-  end
-
-  Sidekiq.configure_client do |config|
-    # config.redis = Secrets.sidekiq.redis.symbolize_keys
-  end
-else
+unless Rails.env.production?
   require 'sidekiq/testing/inline'
   Sidekiq::Testing.inline!
 end
